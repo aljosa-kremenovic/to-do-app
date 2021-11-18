90607fd564ce1bb586cdc4bb8c3b84f60d9ac05b
aljosa-kremenovic <aljosa.kremenovic@gmail.com>
2021-11-18 19:07:47 +0100

    remove code

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -32,39 +32,4 @@ const InputTodo = (props) => {
   );
 };
 
-/* class InputTodo extends Component {
-  state = {
-    title: '',
-  };
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: '',
-    });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          className="input-text"
-          type="text"
-          placeholder="Add Todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <button className="input-submit">Submit</button>
-      </form>
-    );
-  }
-} */
-
 export default InputTodo;
